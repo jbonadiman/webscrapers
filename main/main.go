02438acd4873090e388663c7dd65a9ce67192f90
João Vitor Bonadiman <joao.bonadiman@copastur.com.br>
2023-02-28 00:23:40 -0300

    adds a few more fractions and updates the code to receive the browserless token

--- main/main.go
+++ main/main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"log"
-
-	"humblebundle-scraper/internal"
-)
-
 func main() {
-	recipe := internal.GetRecipe(
-		"https://thewoksoflife.com/jasmine-shortbread-cookies/",
-	)
-
-	log.Println(recipe)
-	log.Println(recipe.PrepTime)
 }
